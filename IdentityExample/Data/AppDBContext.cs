2c64daef52e72fb57bd521fd1211ef4771ddf364
Eduardo Campos <eduardo_c.s@outlook.com>
2020-10-12 14:05:22 -0500

    Database in Memory

---
+++ IdentityExample/Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Data
+{
+    /// <summary>
+    /// Identity contains all the user tables
+    /// </summary>
+    public class AppDBContext:IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options){}
+    }
+}
