9d639c8ae7e41550def262bc27317bf1a4d4d3fe
Eduardo Campos <eduardo_c.s@oulook.com>
2021-02-03 18:52:53 -0600

    Generación de token

---
+++ Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string ISSUER = AUDIANCE;
+        public const string AUDIANCE = @"https://localhost:44352/";
+        public const string SECRET = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
