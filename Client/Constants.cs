c7eeb4c33de8753fcf5ed9191eb145721b3153dc
Eduardo Campos <eduardo_c.s@oulook.com>
2021-03-04 18:48:52 -0600

    Full part 6

---
+++ Client/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class Constants
+    {
+        public const string ISSUER = AUDIANCE;
+        public const string AUDIANCE = @"https://localhost:44373/";
+        public const string SECRET = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
